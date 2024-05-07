ff482edf40689229a45c5d28c0239748d260d3c4
carlos <carlosorantes77@gmail.com>
2024-05-07 18:24:59 -0400

    Starting the authentication services

---
+++ services/AuthenticationService/Controllers/SignInController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    [Route("[controller]")]
+    public class SignInController: ControllerBase
+    {
+        [HttpGet]
+        public void UserLogin() { 
+        
+            
+        }
+    }
+}
---
+++ services/AuthenticationService/Controllers/SignUpController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    [Route("[controller]")]
+    public class SignUp: ControllerBase
+    {
+        [HttpGet]
+        public string AddNewUser() {
+
+            return "tu usuario sera creado";
+        }
+       
+    }
+}
