e9e41ed270783278d3e89543ba47dd3f85016029
carlos <carlosorantes77@gmail.com>
2024-07-08 14:54:19 -0400

    Created a registration DTO to receive only relevant data from the user for creating his account.

---
+++ services/AuthenticationService/DTO/UserRegistrationDto.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationService.DTO
+{
+    public class UserRegistrationDto
+    {
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [MaxLength(45)] 
+        public string Password { get; set; }
+
+        [MaxLength(50)] 
+        public string Nombre { get; set; }
+
+        [MaxLength(50)]
+        public string? Apellido { get; set; }
+    }
+}
