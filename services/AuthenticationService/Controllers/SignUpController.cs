ae738001de9d0f706ffefd84f2e32aebd7022fcb
carlos <carlosorantes77@gmail.com>
2024-07-12 01:48:53 -0400

    modifying User controller for inserting users.

--- services/AuthenticationService/Controllers/SignUpController.cs
+++ services/AuthenticationService/Controllers/SignUpController.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AuthenticationService.DTO;
+using AuthenticationService.Services.Interfaces;
+using AutoMapper;
+using AuthenticationService.Models;
 
 namespace AuthenticationService.Controllers
 {
+    [ApiController]
     [Route("[controller]")]
-    public class SignUp: ControllerBase
+    public class SignUpController: ControllerBase
     {
-        [HttpGet]
-        public string AddNewUser() {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
 
-            return "tu usuario sera creado";
+        public SignUpController(IUserService userService, IMapper mapper) { 
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(UserRegistrationDto userRegistrationDto) {
+
+            var newUser = _mapper.Map<User>(userRegistrationDto);
+
+            _userService.RegisterNewUser(newUser);
+
+            return Created("", userRegistrationDto);
         }
        
     }
