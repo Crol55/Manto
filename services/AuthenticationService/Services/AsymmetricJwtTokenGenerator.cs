ebfd3c3a1f2b3cb25d73c98cdef4a9b492cb007c
carlos <carlosorantes77@gmail.com>
2024-07-21 15:35:34 -0400

    Adding static constructor for Asymmetric JWT
    Now the private.xml file will only be read once, no matter how many instances are created.

--- services/AuthenticationService/Services/AsymmetricJwtTokenGenerator.cs
+++ services/AuthenticationService/Services/AsymmetricJwtTokenGenerator.cs
@@ -10,29 +10,33 @@ namespace AuthenticationService.Services
     public class AsymmetricJwtTokenGenerator : IJwtTokenGenerator
     {
         private readonly JwtOptions _jwtOptions;
+        private static readonly SigningCredentials _asymmetricSigningCredentials;
         public AsymmetricJwtTokenGenerator(IOptions<JwtOptions> jwtOptions) {
             _jwtOptions = jwtOptions.Value;
+        }
+
+        // static constructor, will be executed ONCE per instance.
+        static AsymmetricJwtTokenGenerator() {
+
+            string privateKey = File.ReadAllText("private.xml");
+
+            RSA rsa = RSA.Create();
+            rsa.FromXmlString(privateKey);
+
+            var rsaSecurityKey = new RsaSecurityKey(rsa);
+
+            _asymmetricSigningCredentials = new SigningCredentials(
+                rsaSecurityKey, 
+                SecurityAlgorithms.RsaSha256);
         }
 
 
         public string GenerateToken(string email)
         {
-            
-            var privateKey = File.ReadAllText("private.xml"); 
-            
-            var rsa = RSA.Create();
-            rsa.FromXmlString(privateKey);
-
-            // pass it to RSAsecuritykey
-            var rsaSecurityKey = new RsaSecurityKey(rsa);
-
-            var asymmetricSigningCredentials = new SigningCredentials(
-                rsaSecurityKey, SecurityAlgorithms.RsaSha256);
-
             var jwt = new JwtSecurityToken(
                 issuer: _jwtOptions.Issuer,
                 expires: DateTime.UtcNow.AddMinutes(_jwtOptions.ExpirationInMinutes),
-                signingCredentials: asymmetricSigningCredentials
+                signingCredentials: _asymmetricSigningCredentials
             );;
 
             return new JwtSecurityTokenHandler().WriteToken(jwt);
