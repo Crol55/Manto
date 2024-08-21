359de2d3819bb3bd93e57b189388dd3d6bb17fd4
carlos <carlosorantes77@gmail.com>
2024-08-21 15:10:10 -0400

    Adding a controller to allow an authenticated user to create a new project

--- services/BoardsService/Controllers/ProjectsController.cs
+++ services/BoardsService/Controllers/ProjectsController.cs
@@ -2,11 +2,15 @@
 using BoardsService.DTO;
 using BoardsService.Models;
 using BoardsService.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using ServiceExceptionsLibrary;
 
 namespace BoardsService.Controllers;
 
     [Route("[controller]")]
     [ApiController]
+    [Authorize]
     public class ProjectsController: ControllerBase
     {
         private readonly IProjectService _projectService;
@@ -18,14 +22,20 @@ namespace BoardsService.Controllers;
         [HttpPost]
         public IActionResult CreateNewProject(ProjectRequestDto projectRequestDto) {
 
-            Project newProject = new() {
+            string? userId = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            
+            if (string.IsNullOrEmpty(userId))
+                throw new ValidationException($"The claim {JwtRegisteredClaimNames.Sub} was not provided");
+            
+
+            Project newProject = new() 
+            {
                 Nombre = projectRequestDto.Nombre,
-                UserId = "1b390c20-234e-4b42-9f19-19d1acfd5d00"
+                UserId = userId
             };
             
             _projectService.AddNewProject(newProject);
             
-
             return Created();
         }
 
