289c58b1ac1481103b60270a4fd4882178a7fe86
carlos <carlosorantes77@gmail.com>
2024-08-27 00:45:59 -0400

    Adding JWT nuget package to the project

---
+++ services/BoardsService/Controllers/BoardsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoardsService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BoardsController: ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateBoard() {
+
+            return Ok("your board will be created");
+        }
+    }
+}
