35be5cee1cc58499e14ee63d314da696a4fa6877
carlos <carlosorantes77@gmail.com>
2025-04-30 12:43:32 -0400

    Add DTO to handle data received from the request body and prevent under/over-posting.

---
+++ services/BoardsService/DTO/Extensions/DtoExtensions.cs
@@ -0,0 +1,16 @@
+﻿using BoardsService.Models;
+
+namespace BoardsService.DTO.Extensions
+{
+    public static class DtoExtensions
+    {
+        public static Board ToBoard(this BoardCreateDto boardCreateDto) {
+
+            return new Board()
+            {
+                Name = boardCreateDto.Name,
+                ProjectId = boardCreateDto.ProjectId
+            };
+        }
+    }
+}
