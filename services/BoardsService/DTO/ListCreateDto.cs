9829ec15c1fef7700e1b25625120aced095d1236
carlos <carlosorantes77@gmail.com>
2025-05-27 00:48:29 -0400

    Add DTO to handle Board List creation request

---
+++ services/BoardsService/DTO/ListCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardsService.DTO
+{
+    public class ListCreateDto
+    {
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public int? Position { get; set; } 
+
+        [Required]
+        public Guid? BoardId { get; set; }
+    }
+}
