b645ce684e551f43b23450cc215d2e2c304654e2
carlos <carlosorantes77@gmail.com>
2025-05-31 22:38:24 -0400

    Add BoardMemberService to verify user permissions within a board and map database roles to Enum equivalents

---
+++ services/BoardsService/Services/BoardMemberService.cs
@@ -0,0 +1,45 @@
+﻿using BoardsService.Data;
+using BoardsService.Models;
+using BoardsService.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using BoardsService.Common.Enums;
+
+namespace BoardsService.Services
+{
+    public class BoardMemberService : IBoardMemberService
+    {
+        private readonly BoardsDbContext _dbContext;
+
+        public BoardMemberService(BoardsDbContext dbContext) { 
+            _dbContext = dbContext;
+        }
+
+        public bool VerifyUserMembership(Guid boardId, Guid userId)
+        {
+            return _dbContext.BoardMembers.Any(m => m.BoardId == boardId && m.UserId == userId);
+        }
+
+        public Roles GetUserRole (Guid userId, Guid boardId)
+        {
+            BoardMember? member = _dbContext.BoardMembers
+                .Include( m => m.Role)
+                .FirstOrDefault(m => (m.BoardId == boardId) && (m.UserId == userId));
+
+            return member == null 
+                ? Roles.None 
+                : MapToRole(member.Role.RoleName);
+        }
+
+        private Roles MapToRole(string databaseRole) 
+        {
+            return databaseRole switch
+            {
+                "Owner"         => Roles.Owner,
+                "Admin"         => Roles.Admin,
+                "Collaborator"  => Roles.Collaborator,
+                "Viewer"        => Roles.Viewer,
+                _               => Roles.None,
+            };
+        }
+    }
+}
---
+++ services/BoardsService/Services/Interfaces/IBoardMemberService.cs
@@ -0,0 +1,13 @@
+﻿using BoardsService.Common.Enums;
+
+namespace BoardsService.Services.Interfaces
+{
+    public interface IBoardMemberService
+    {
+        //void AddMember(Guid boardId, Guid priviligedUserId, Guid targetUserId, string roleToAssign);
+
+        bool VerifyUserMembership(Guid boardId, Guid userId);
+
+        Roles GetUserRole(Guid userId, Guid boardId);
+    }
+}
