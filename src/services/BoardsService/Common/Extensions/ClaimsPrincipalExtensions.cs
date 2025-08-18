12039bde2148150b35a11b8beccfe138b54b31c3
carlos <carlosorantes77@gmail.com>
2025-08-18 18:51:54 -0400

    Add an extension method for reading the User's JWT.

---
+++ src/services/BoardsService/Common/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using ServiceExceptionsLibrary;
+
+namespace BoardsService.Common.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+
+        public static string GetUserIdOrThrow(this ClaimsPrincipal user) 
+        {
+            string? userId = user.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+                throw new ValidationException($"The claim {JwtRegisteredClaimNames.Sub} was not provided");
+
+            return userId;
+        }
+    }
+}
