eed966feb7fbf50dbe70d3c6ce10bd11f9ba2405
carlos <carlosorantes77@gmail.com>
2025-09-22 18:57:54 -0400

    Add both the ability to update a single list and also retrieve all Lists associated with a Board.

--- src/services/BoardsService/Services/ListService.cs
+++ src/services/BoardsService/Services/ListService.cs
@@ -13,7 +13,7 @@ namespace BoardsService.Services
         private readonly BoardsDbContext _dbContext;
         private readonly IBoardMemberService _boardMemberService;
 
-        public ListService(BoardsDbContext boardsDbContext, IBoardMemberService boardMemberService) 
+        public ListService(BoardsDbContext boardsDbContext, IBoardMemberService boardMemberService)
         {
             _dbContext = boardsDbContext;
             _boardMemberService = boardMemberService;
@@ -29,14 +29,14 @@ namespace BoardsService.Services
         public BoardList AddList(ListCreateDto dto, Guid RegisteredUserId)
         {
             BoardList newBoardList = dto.ToBoardList();
-                newBoardList.UpdatedAt = DateTime.Now;
+            newBoardList.UpdatedAt = DateTime.Now;
 
             Roles userRole = _boardMemberService.GetUserRole(RegisteredUserId, newBoardList.BoardId);
             if (userRole == Roles.None)
                 throw new ForbiddenAccessException("You dont have access to this Resource");
-                
+
             if (_dbContext.BoardLists.Any(
-                l => l.Name == newBoardList.Name && 
+                l => l.Name == newBoardList.Name &&
                 l.BoardId == newBoardList.BoardId))
                 throw new DuplicatedEntryException($"The list:'{newBoardList.Name}' already exists in the Board");
 
@@ -52,6 +52,24 @@ namespace BoardsService.Services
                 Console.WriteLine(ex.ToString());
             }
             return newBoardList;
+        }
+
+
+        public IEnumerable<BoardList> GetAllLists(Guid boardId) => 
+            this._dbContext.BoardLists.Where(bl => bl.BoardId == boardId).ToList();
+
+
+        public void UpdateList(Guid listId, ListUpdateDto newListValues) {
+
+            BoardList listToUpdate =  this._dbContext.BoardLists
+                .SingleOrDefault(x => x.Id == listId) 
+                ?? throw new Exception();// if null, it means they are accessing a non-existent resource
+
+            listToUpdate.Name = newListValues.Name ?? listToUpdate.Name;
+            listToUpdate.Position = newListValues.Position;// remember to conver short to short?  ?? listToUpdate.Position;
+            listToUpdate.UpdatedAt = newListValues.UpdatedAt;
+
+             this._dbContext.SaveChanges();
         }
     }
 }
